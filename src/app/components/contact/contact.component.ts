066ff3780fcc009cd10435dedaa36ea2eba8a220
Boitumelo-perez <perez_st@icloud.com>
2025-01-18 22:48:29 +0200

    all sections somewhat implemented

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMapMarkerAlt, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-contact',
-    imports: [],
+    imports: [FontAwesomeModule],
     templateUrl: './contact.component.html',
     styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+    faMapMarkerAlt = faMapMarkerAlt;
+    faEnvelope = faEnvelope;
+    faPhone = faPhone;
 }
