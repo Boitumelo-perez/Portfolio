f0053abfc77154a624b32078e8fa3b829fe193a0
Boitumelo-perez <perez_st@icloud.com>
2025-06-04 03:32:14 +0200

    Dock files

--- Django Backend/portfolio_backend/settings.py
+++ Django Backend/portfolio_backend/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'portfolio_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'user',
+        'PASSWORD': '1password',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
